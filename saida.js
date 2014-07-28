03b5d400aae71918115148a4d4f75c9410ee3ffa
talyssonoc <talyssonoc@gmail.com>
2014-07-28 14:56:33 -0300

    Delete file commited by mistake

--- saida.js
+++
@@ -1,3 +0,0 @@
-(function(){var User=Backbone.Model.extend({"idAttribute":"id","urlRoot":"/data/User","defaults":{}});var UserCollection=Backbone.Collection.extend({model:User,url:'/data/User'});if(typeof module!=='undefined'&&module.exports){module.exports={model:User,collection:UserCollection};}
-else if(typeof window.define==='function'&&window.define.amd){define(function(){return{model:User,collection:UserCollection};});}
-else{window.User=User;window.UserCollection=UserCollection;}}());
